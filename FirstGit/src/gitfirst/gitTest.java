893a6c6d2946b5b3908aba15c819e8e0893f7449
Mohamed <mohamed_94@live.at>
2013-05-23 15:54:56 +0200

    first commit!

---
+++ FirstGit/src/gitfirst/gitTest.java
@@ -0,0 +1,13 @@
+package gitfirst;
+
+public class gitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("First Git!");
+	}
+
+}
